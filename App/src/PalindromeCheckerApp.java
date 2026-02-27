d3c473b3758aaf141ff8718c52fc08897658d083
jagan0115 <jaganff15@gmail.com>
2026-02-27 10:14:30 +0530

    jagan Added UseCase2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,7 +2,7 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "madam";
+        String input = "sir";
         String reverse = "";
 
         for (int i = input.length() - 1; i >= 0; i--) {
