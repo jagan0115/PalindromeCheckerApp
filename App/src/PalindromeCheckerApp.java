e144bc65c57a6c3a0fc98c4b98b3a604071c3a04
jagan0115 <jaganff15@gmail.com>
2026-02-27 10:27:17 +0530

    jagan Added UseCase3

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,17 +2,20 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "sir";
+        // Input string
+        String input = "madam";
         String reverse = "";
 
+        // Reverse the string using for loop
         for (int i = input.length() - 1; i >= 0; i--) {
             reverse = reverse + input.charAt(i);
         }
 
+        // Compare original and reversed strings
         if (input.equals(reverse)) {
-            System.out.println(input + " is a Palindrome");
+            System.out.println("The string \"" + input + "\" is a Palindrome");
         } else {
-            System.out.println(input + " is Not a Palindrome");
+            System.out.println("The string \"" + input + "\" is Not a Palindrome");
         }
     }
 }
